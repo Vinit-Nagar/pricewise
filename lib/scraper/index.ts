51b547afe5f6a13e76dbf3f4df6da2422f412a77
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-03 19:29:31 +0530

    scraping setup

---
+++ lib/scraper/index.ts
@@ -0,0 +1,20 @@
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_PASSWORD);
+  const port = 22225;
+  const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: "brd.superproxy.io",
+    port,
+    rejectUnauthorized: false,
+  };
+  try {
+  } catch (error: any) {
+    throw new Error(`Failed to scrape the product: ${error.message}`);
+  }
+}
