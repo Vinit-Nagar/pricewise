aff825314b26428c3424c8d662ec47c164f17b6b
Vinit-Nagar <vinitnagar21@gmail.com>
2024-06-11 17:46:19 +0530

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
   getLowestPrice,
 } from "@/lib/utlis";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 59; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
